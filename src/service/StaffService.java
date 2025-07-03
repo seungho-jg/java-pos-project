2ed46cdeaf3e0d56cdb84e79bac6dc2e84549744
seungho <24528@naver.com>
2025-07-03 16:02:15 +0900

    Feat: StaffService 로직 구현 - staffDao를 생성하고 getStaff()를 통해 사용자 정보를 조회한다. - 관심사별로 분리하여 staff에 관련된 서비스 로직을 여기에 구현할 예정이다.

---
+++ src/service/StaffService.java
@@ -0,0 +1,22 @@
+package service;
+
+import db.StaffDao;
+import model.Staff;
+
+public class StaffService {
+    StaffDao staffDao = null; // 싱글톤
+
+    public String login(String id, String pw){
+        Staff result = staffDao.getStaff(id, pw);
+
+        if (result == null) {
+            System.out.println("아이디나 비밀번호가 틀렸습니다.");
+            return "";
+        }
+        return result.name();
+    }
+
+    public StaffService() {
+        staffDao = new StaffDao();
+    }
+}
