cb5eeea5a924c0c29c810c5a1a4dc19d997844b2
seungho <24528@naver.com>
2025-07-07 09:30:08 +0900

    Feat: StoreDao 추가

--- src/db/storeDao.java
+++ src/db/storeDao.java
@@ -1,5 +1,22 @@
 package db;
 
-public class storeDao {
+public class StoreDao {
+    final private int storeId = 1;
+    private int balance = 1234000;
 
+    public int getStoreId() {
+        return this.storeId;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public void increaseBalance(int amount) {
+        this.balance += amount;
+    }
+
+    public void decreaseBalance(int amount) {
+        this.balance -= amount;
+    }
 }
